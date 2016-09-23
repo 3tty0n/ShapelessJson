8aaa1664ce3db88d8bd63b5b0fe7e8e22bdf29ac
YuIzawa <yuizalp@gmail.com>
2016-09-23 16:25:35 +0900

    fix Writes test

--- src/test/scala/ShapelessJsonTest.scala
+++ src/test/scala/ShapelessJsonTest.scala
@@ -1,23 +1,21 @@
 import org.scalatest._
 import play.api.libs.json._
-import shapeless._
 
 class ShapelessJsonTest
   extends FlatSpec
   with Matchers {
 
-  "SWrites" should "Json を Writeできる" in new ShapelessJson {
+  "SWrites" should "Json を Write できる" in new ShapelessJson {
     case class Foo(name: String, number: Int)
 
     object Foo {
       implicit val writes: Writes[Foo] = SWrites.deriveInstance
     }
 
-    val json = Json.parse(
-      """
-        |{"name": "foo",
-        |"number": 12
-        |}""".stripMargin)
+    val json = Json.parse(""" {
+      | "name": "foo",
+      | "number": 12
+      |}""".stripMargin)
 
 
     assert(Json.toJson(Foo("foo", 12)) === json)
